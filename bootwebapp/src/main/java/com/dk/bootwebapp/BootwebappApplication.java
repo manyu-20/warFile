4b286cc4ef37650341a46bfc4477e271af4fed6a
Abhimanyu Saini <abhimanyusaini@Abhimanyus-MacBook-Air.local>
2023-10-26 14:33:33 +0530

    initial commit

---
+++ bootwebapp/src/main/java/com/dk/bootwebapp/BootwebappApplication.java
@@ -0,0 +1,13 @@
+package com.dk.bootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootwebappApplication.class, args);
+	}
+
+}
---
+++ bootwebapp/src/test/java/com/dk/bootwebapp/BootwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.bootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
